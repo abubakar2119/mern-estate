140b4126c077d8ef4b4831c96d94fbf8c6e8c8ea
abubakar2119 <your-github-email@example.com>
2026-08-21 05:43:01 +0500

    fix vercel express deployment

--- api/index.js
+++ api/index.js
@@ -1,22 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB!');
-  })
-  .catch((err) => {
-    console.log('MongoDB connection error:', err);
-  });
 
 const app = express();
 
@@ -27,15 +17,13 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error:', err);
   });
-});
 
 export default app;
